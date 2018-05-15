d6a1e68ae34889817bcc284a49c99a622456476b
Denasius <den_kostyuk1989@mail.ru>
2018-05-15 23:19:24 +0300

    added new files

---
+++ dist/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.div-items').slick({		
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dotsClass: 'slick-dots-nav',
+		speed: 300
+	});
+});
